1893d0992f091170843fcbf9637f73717fac414f
Nicolas Ardison <nicolas.ard@gmail.com>
2020-11-08 22:30:47 -0300

    First commit

---
+++ SpringSecurity_Example1/src/main/java/ar/nic/springsecurity/controller/HealthController.java
@@ -0,0 +1,17 @@
+package ar.nic.springsecurity.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "ok";
+    }
+
+}
