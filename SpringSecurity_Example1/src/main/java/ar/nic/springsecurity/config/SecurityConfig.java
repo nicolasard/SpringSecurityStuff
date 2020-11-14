a219e4ba061f32825d2d8bd168a0ae7685fd89ee
Nicolas Ardison <nicolas.ard@gmail.com>
2020-11-14 18:14:51 -0300

    Adding secured page in project

--- SpringSecurity_Example1/src/main/java/ar/nic/springsecurity/config/SecurityConfig.java
+++ SpringSecurity_Example1/src/main/java/ar/nic/springsecurity/config/SecurityConfig.java
@@ -22,7 +22,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login")
                 .permitAll()
                 .and()
                 .logout()
