da38912efe712d94e246bf9ed815cf1378c31ef4
faloggs <Nicolas.Ardison@firstdata.com.ar>
2020-11-23 20:35:58 -0300

    Adding example 4

---
+++ SpringSecurity_Example4/src/main/java/ar/nic/springsecurity/entity/ConfirmationToken.java
@@ -0,0 +1,40 @@
+package ar.nic.springsecurity.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class ConfirmationToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String confirmationToken;
+
+    private LocalDate createdDate;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    public ConfirmationToken(User user){
+        this.user = user;
+        this.createdDate = LocalDate.now();
+        this.setConfirmationToken(UUID.randomUUID().toString());
+    }
+
+    public String getConfirmationToken() {
+        return confirmationToken;
+    }
+
+    public void setConfirmationToken(String confirmationToken) {
+        this.confirmationToken = confirmationToken;
+    }
+}
---
+++ SpringSecurity_Example4/src/main/java/ar/nic/springsecurity/services/EmailSenderService.java
@@ -0,0 +1,19 @@
+package ar.nic.springsecurity.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@Service
+@AllArgsConstructor
+public class EmailSenderService {
+
+    private JavaMailSender javaMailSender;
+
+    @Async
+    public void sendEmail(SimpleMailMessage email) {
+        javaMailSender.send(email);
+    }
+}
