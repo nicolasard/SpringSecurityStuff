58da1ee808e4a9c89ae25430b8e3ee8fc22a5b21
faloggs <Nicolas.Ardison@firstdata.com>
2021-07-18 23:01:12 -0300

    - Adding sing up form validations - Adding Bootstrap to the sing up form

--- SpringSecurity_Example4/src/main/java/ar/nic/springsecurity/controller/UserController.java
+++ SpringSecurity_Example4/src/main/java/ar/nic/springsecurity/controller/UserController.java
@@ -6,10 +6,12 @@ import ar.nic.springsecurity.services.ConfirmationTokenService;
 import ar.nic.springsecurity.services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @Controller
@@ -33,8 +35,11 @@ public class UserController {
     }
 
     @PostMapping("/sign-up")
-    String signUp(User user) {
+    String signUp(@Valid User user, BindingResult bindingResult) {
 
+        if (bindingResult.hasErrors()) {
+            return "sign-up2";
+        }
         userService.signUpUser(user);
 
         return "redirect:/sign-in2";
--- SpringSecurity_Example4/src/main/java/ar/nic/springsecurity/entity/User.java
+++ SpringSecurity_Example4/src/main/java/ar/nic/springsecurity/entity/User.java
@@ -9,6 +9,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -25,8 +28,12 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
+    @Size(min=2, max=30)
     private String name;
 
+    @NotNull
+    @Size(min=2, max=30)
     private String surname;
 
     public void setName(String name) {
@@ -45,8 +52,12 @@ public class User implements UserDetails {
         this.userRole = userRole;
     }
 
+    @NotNull
+    @Email
     private String email;
 
+    @NotNull
+    @Size(min=5, max=30)
     private String password;
 
     @Builder.Default
