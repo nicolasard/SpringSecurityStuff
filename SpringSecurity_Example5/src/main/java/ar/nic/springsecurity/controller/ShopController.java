c9f9a559412d430962cf1974d24d6b342dd17af7
Nicolas Ardison <nicolas.ard@gmail.com>
2021-07-25 19:59:21 -0300

    Minor changes

--- SpringSecurity_Example5/src/main/java/ar/nic/springsecurity/controller/ShopController.java
+++ SpringSecurity_Example5/src/main/java/ar/nic/springsecurity/controller/ShopController.java
@@ -1,23 +1,25 @@
 package ar.nic.springsecurity.controller;
 
 import ar.nic.springsecurity.entity.Payment;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.View;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.xml.transform.stream.StreamResult;
-import java.io.PrintWriter;
-import java.util.Enumeration;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("shop")
 public class ShopController {
+
+    @GetMapping("billing")
+    String bills() {
+        return "billing";
+    }
 
     @GetMapping("product")
     String product() {
@@ -26,23 +28,22 @@ public class ShopController {
 
     @GetMapping("card-payment")
     String cardPaymentGet(Payment payment) {
-        payment.setTxntype("preauth");
         return "card-payment";
     }
 
     @PostMapping("card-payment")
-    String cardPaymentPost(Payment payment, HttpServletRequest request) {
-        payment.setCurrency("999");
-
-        Enumeration e = request.getParameterNames();
-        while (e.hasMoreElements()) {
-            String name = (String)e.nextElement();
-            String value = request.getParameter(name);
-            System.out.println(name + " = " + value);
-        }
-
-        request.setAttribute(
-                View.RESPONSE_STATUS_ATTRIBUTE, HttpStatus.TEMPORARY_REDIRECT);
-        return "redirect:https://webhook.site/4533b71e-9286-44dc-aa03-b925e83ce57f";
+    String cardPaymentPost(Payment payment) {
+        UUID uuid = UUID.randomUUID();
+        DateFormat df = new SimpleDateFormat("yyyy:MM:dd-kk:mm:ss");
+        payment.setChargetotal("13.99");
+        payment.setTxntpe("sale");
+        payment.setOid(uuid.toString());
+        payment.setCurrency("EUR");
+        payment.setTimezone(TimeZone.getDefault().getID());
+        payment.setTxdatetime(df.format(new Date()));
+        payment.setStorename("120995000");
+        payment.setHashExtended(payment.getHash("sharedsecret"));
+        payment.setHash_algorithm("HMACSHA256");
+        return "card-payment-confirm";
     }
 }
--- SpringSecurity_Example5/src/main/java/ar/nic/springsecurity/entity/Payment.java
+++ SpringSecurity_Example5/src/main/java/ar/nic/springsecurity/entity/Payment.java
@@ -1,10 +1,58 @@
 package ar.nic.springsecurity.entity;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.lang.reflect.Field;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
+import java.util.*;
+
 public class Payment {
+
+    private String oid;
+
+    private String chargetotal;
 
     private String currency;
 
-    private String txntype;
+    private String paymentMethod;
+
+    private String responseFailURL;
+
+    private String responseSuccessURL;
+
+    private String sharedsecret;
+
+    private String storename;
+
+    private String timezone;
+
+    private String transactionNotificationURL;
+
+    private String txdatetime;
+
+    private String txntpe;
+
+    private String hashExtended;
+
+    private String hash_algorithm;
+
+    public String getOid() {
+        return oid;
+    }
+
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
+
+    public String getChargetotal() {
+        return chargetotal;
+    }
+
+    public void setChargetotal(String chargetotal) {
+        this.chargetotal = chargetotal;
+    }
 
     public String getCurrency() {
         return currency;
@@ -14,12 +62,144 @@ public class Payment {
         this.currency = currency;
     }
 
-    public String getTxntype() {
-        return txntype;
+    public String getPaymentMethod() {
+        return paymentMethod;
     }
 
-    public void setTxntype(String txntype) {
-        this.txntype = txntype;
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
     }
 
+    public String getResponseFailURL() {
+        return responseFailURL;
+    }
+
+    public void setResponseFailURL(String responseFailURL) {
+        this.responseFailURL = responseFailURL;
+    }
+
+    public String getResponseSuccessURL() {
+        return responseSuccessURL;
+    }
+
+    public void setResponseSuccessURL(String responseSuccessURL) {
+        this.responseSuccessURL = responseSuccessURL;
+    }
+
+    public String getSharedsecret() {
+        return sharedsecret;
+    }
+
+    public void setSharedsecret(String sharedsecret) {
+        this.sharedsecret = sharedsecret;
+    }
+
+    public String getStorename() {
+        return storename;
+    }
+
+    public void setStorename(String storename) {
+        this.storename = storename;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
+
+    public String getTransactionNotificationURL() {
+        return transactionNotificationURL;
+    }
+
+    public void setTransactionNotificationURL(String transactionNotificationURL) {
+        this.transactionNotificationURL = transactionNotificationURL;
+    }
+
+    public String getTxdatetime() {
+        return txdatetime;
+    }
+
+    public void setTxdatetime(String txdatetime) {
+        this.txdatetime = txdatetime;
+    }
+
+    public String getTxntpe() {
+        return txntpe;
+    }
+
+    public void setTxntpe(String txntpe) {
+        this.txntpe = txntpe;
+    }
+
+    public String getExtendedConcat() throws IllegalAccessException {
+        Map<String, String> dictionary = new HashMap<String, String>();
+        for (Field f : getClass().getDeclaredFields()) {
+            Object field = f.get(this);
+            if (field != null) {
+                if (!field.toString().isEmpty() && !f.getName().equals("sharedsecret")) {
+                    dictionary.put(f.getName(), f.get(this).toString());
+                }
+            }
+        }
+
+        List<String> employeeByKey = new ArrayList<>(dictionary.keySet());
+        Collections.sort(employeeByKey);
+        StringJoiner sj = new StringJoiner("|");
+        for(String s : employeeByKey){
+            sj.add(dictionary.get(s).toString());
+        }
+        return sj.toString();
+    }
+
+    public String getHash(String sharedScret) {
+        try {
+            return calculateHMAC(this.getExtendedConcat(),sharedScret);
+        } catch (SignatureException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static String toHexString(byte[] bytes) {
+        Formatter formatter = new Formatter();
+        for (byte b : bytes) {
+            formatter.format("%02x", b);
+        }
+        return formatter.toString();
+    }
+
+    public static String calculateHMAC(String data, String key)
+            throws SignatureException, NoSuchAlgorithmException, InvalidKeyException {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(), "HmacSHA256");
+        Mac mac = Mac.getInstance("HmacSHA256");
+        mac.init(secretKeySpec);
+        String base64HmacSha256 = Base64.getEncoder().encodeToString(mac.doFinal(data.getBytes()));
+
+        return base64HmacSha256;
+    }
+
+    public String getHashExtended() {
+        return hashExtended;
+    }
+
+    public void setHashExtended(String hashExtended) {
+        this.hashExtended = hashExtended;
+    }
+
+    public String getHash_algorithm() {
+        return hash_algorithm;
+    }
+
+    public void setHash_algorithm(String hash_algorithm) {
+        this.hash_algorithm = hash_algorithm;
+    }
 }
